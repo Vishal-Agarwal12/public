f4e98f22ab9a0e0a248ba0651f02dcbaa53090b8
Vishal Agarwal <vishalagarwal5720@gmail.com>
2021-12-16 13:38:59 +0530

    Initial Commit

---
+++ js/product.js
@@ -0,0 +1,31 @@
+const productImages = document.querySelectorAll(".product-images img");
+const productImageSlide = document.querySelector(".image-slider");
+
+let activeImageSlide = 0;
+
+productImages.forEach((item, i) => {
+    item.addEventListener('click', () => {
+
+        productImages[activeImageSlide].classList.remove('active');
+
+        item.classList.add('active');
+        console.log(item.src);
+        let sr= item.src;
+        // console.log(sr);
+       productImageSlide.style.backgroundImage = "url"+"("+sr+")";
+        activeImageSlide = i;
+
+    })
+})
+
+const sizeBtns = document.querySelectorAll('.size-radio-btn');
+let checkedBtn = 0;
+
+sizeBtns.forEach((item, i) => {
+    item.addEventListener('click', () => {
+        sizeBtns[checkedBtn].classList.remove('check');
+        item.classList.add('check');
+        checkedBtn = i;
+    })
+
+})
